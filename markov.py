583b02c0d938ecd1853f4eadc9c5e4f4650ed2ea
ChrUbuntu User <user@chromebox-003.(none)>
2013-03-14 18:17:08 -0700

    with Christian's changes to use .pop, .setdefault, and random.choices

--- markov.py
+++ markov.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 
 import sys
-from random import randint
+import random
 
-def make_chains(corpus):
+def make_chains(corpus, ngram_size):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""    
-
-    markov_chains_dict = {}
 
     # To do: strip out cruft later
 
     # break up input string of text into a list of individual words
     word_list = corpus.split() 
 
-    # loop through list of words by index, set a tuple as a new key in the dictionary and the third word as the value. End before the last two words in the list.
-    for i in range(len(word_list)-2):
+    # this is the first ngram that will become a key in the dictionary.
+    # getting a slice of the word_list from the beg of the list, with the max size passed into this func at ngram_size; converted to a tuple for use as a key in the dict
+    n_gram = tuple(word_list[:ngram_size])
+    
+    # delete set of words being used as the first key (n_gram)
+    del word_list[:ngram_size]
 
-        bigram = word_list[i], word_list[i+1]
-        value = word_list[i+2]
+    markov_chains_dict = {}
 
-        # To do: is there any way to shorten this using the get method for dicts?
-        
-        # if word pair doesn't already exist in the dictionary, add it as a new key and give it the third word as the value in a *list* 
-        if bigram not in markov_chains_dict:
-            markov_chains_dict[bigram] = [value]
-
-        # if word pair is already in the dictionary (not unique), then append the third word to existing list of values
-        else:
-            markov_chains_dict[bigram].append(value)
+    # while word_list still exists and has words in it
+    while word_list:
+        # taking the first word off the rest of the list
+        word = word_list.pop(0)
+        # looks for key in the dict, if it exists, then append word to existing list of values; if key is not in dict, set it in the dictionary with an empty list as the value, then add word to that empty list
+        markov_chains_dict.setdefault(n_gram,[]).append(word)
+        # set new n_gram key based on previous n_gram, starting from the 2nd item to the end of the previous n_gram, and tack on the just added word as part of the tuple
+        n_gram = n_gram[1:] + (word,)
 
     return markov_chains_dict
 
@@ -36,42 +36,42 @@ def make_text(chains, max_length):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
     
-    # add all keys from chains dict to new list
-    convert_dict_list = []
-    for keys in chains:
-        convert_dict_list.append(keys)
+    # grab a random key from the chains dict, this is a tuple. Could be last set of words in the text??? Doesn't seem right.
+    seed = random.choice(chains.keys())
 
-    # generate a random index number to pick out a tuple from the convert_dict_list
-    rand_index = randint(0, len(convert_dict_list)-1)
+    # convert the tuple into a list and add it to the list random_text_list
+    random_text_list = []
+    random_text_list += list(seed)
 
-    # add the first and second items in that tuple as separate items to the convert_dict_list
-    random_text_list = [convert_dict_list[rand_index][0], convert_dict_list[rand_index][1]]
-    
-    # grabs the last two items of the random_text list 
-    last_two = random_text_list[-2],random_text_list[-1]
- 
-    while last_two in chains and len(random_text_list) < max_length:
-        # values are stored in a list, generates a random index number to randomly grab value 
-        rand_index = randint(0, len(chains[last_two])-1)
+    # while the random_text_list is not yet the max length specified...
+    while len(random_text_list) < max_length:
+        # to deal with if the key chosen is the last set
+        choices = None
+        while not choices:
+            choices = chains.get(seed)
+            if not choices:
+                seed = random.choice(chains.keys())
+                random_text_list += list(seed)
 
-        # append value of key matching previous last two words from random_text_list to the list 
-        random_text_list.append(chains[last_two][rand_index])
-        
-        # we've changed the contents of the list, reset last_two variable to be the new last two items in the last
-        last_two = random_text_list[-2],random_text_list[-1]
+        # choose a random value from the list of values in the dictionary for the seed key
+        next = random.choice(chains[seed])
+        # append that new value (word) to the random_text_list
+        random_text_list.append(next)
+        # set new seed key based on previous seed, starting from the 2nd item to the end of the previous seed, and tack on the just added next word as part of the tuple
+        seed = seed[1:] + (next,)
 
     final_string = ' '.join(random_text_list)
 
     return final_string
 
 def main():
-    args = sys.argv
+    script, filename, ngram_size, max_length = sys.argv
 
     # args is a tuple of what's inputted in terminal after python command, therefore can get filename using args[1]
-    input_text = open(args[1]).read()
+    input_text = open(filename).read()
 
-    chain_dict = make_chains(input_text)
-    random_text = make_text(chain_dict, int(args[2]))
+    chain_dict = make_chains(input_text, int(ngram_size))
+    random_text = make_text(chain_dict, int(max_length))
     print random_text
 
 if __name__ == "__main__":
